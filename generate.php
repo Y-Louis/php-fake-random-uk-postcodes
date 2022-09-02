582491d6f2d978b32df8436ba79011ca982b2513
Louis <76131224+cacode-louis@users.noreply.github.com>
2022-09-02 23:33:21 +0100

    Bit buggy, whoops

--- generate.php
+++ generate.php
@@ -7,16 +7,16 @@ function RandomUKPostcode($StyleID = null){
 
     switch($StyleID){
         case 0: // AA9A 9AA
-            return chr(rand(65,90)) . chr(rand(65,90)) . rand(1,50) . chr(rand(65,90)) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
+            return chr(rand(65,90)) . chr(rand(65,90)) . rand(1,9) . chr(rand(65,90)) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
         case 1: // A9A 9AA
-            return chr(rand(65,90)) . rand(1,50) . chr(rand(65,90)) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
+            return chr(rand(65,90)) . rand(1,9) . chr(rand(65,90)) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
         case 2: // A9 9AA
-            return chr(rand(65,90)) . rand(1,50) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
+            return chr(rand(65,90)) . rand(1,9) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
         case 3: // A99 9AA
-            return chr(rand(65,90)) . rand(1,50) . rand(1,50) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
+            return chr(rand(65,90)) . rand(11,50) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
         case 4: // AA9 9AA
-            return chr(rand(65,90)) . chr(rand(65,90)) . rand(1,50) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
+            return chr(rand(65,90)) . chr(rand(65,90)) . rand(1,9) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
         case 5: // AA99 9AA
-            return chr(rand(65,90)) . chr(rand(65,90)) . rand(1,50) . rand(1,50) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
+            return chr(rand(65,90)) . chr(rand(65,90)) . rand(11,50) . " " . rand(1,9) . chr(rand(65,90)) . chr(rand(65,90));
     }
 }
